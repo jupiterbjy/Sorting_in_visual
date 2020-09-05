92759c0846e5975482527dbd8863267ec9457235
jupiterbjy <jupiterbjy@gmail.com>
2020-09-05 13:04:10 +0900

    Now support selection of Visualizing methods.

--- async_main.py
+++ async_main.py
@@ -18,11 +18,6 @@ def get_size():
         # I don't expect someone `mistakenly` call this function up to recursion limit.
 
 
-def select_visualize_method():
-    lists = GetModuleReference.ListFunction(VisualMethod)
-    pass
-
-
 def generate_test(n: int):
     return shuffle([i for i in range(1, n + 1)])
 
@@ -39,13 +34,32 @@ def shuffle(arr: MutableSequence):
     return arr
 
 
-def get_sorts_list() -> list:
-    # show list of sorts implemented
-    sort_list = sorted(Sorting_algorithms_pure.__all__)
-    digit = len(str(len(sort_list)))
+def show_list(items):
+    digit = len(str(len(items)))
 
-    for idx, sort in enumerate(sort_list):
-        print(f"{idx:{digit}} {sort}")
+    for idx, item in enumerate(items):
+        print(f"{idx:{digit}} {item}")
+
+
+def select_visualize_method():
+    visual_list = GetModuleReference.ListFunction(VisualMethod)
+    show_list(visual_list)
+
+    # get visual method
+    raw_input = input("Enter visualizing method index: ")
+    try:
+        selected = getattr(VisualMethod, visual_list[int(raw_input)])
+    except ValueError:
+        print("Wrong index provided, try again.")
+        return select_visualize_method()
+
+    return selected
+
+
+def select_sorts_list() -> list:
+    # show list of sorts implemented
+    sort_list = GetModuleReference.ListFunction(Sorting_algorithms_pure)
+    show_list(sort_list)
 
     # get list of sorts to play
     raw_list = input("Enter multiple index of sorts to play: ")
@@ -55,21 +69,22 @@ def get_sorts_list() -> list:
 
     except IndexError:
         print("Index provided out of range, try again.")
-        return get_sorts_list()
+        return select_sorts_list()
 
     except ValueError:
         print("Wrong index provided, try again.")
-        return get_sorts_list()
+        return select_sorts_list()
 
     else:
         return selected_list
 
 
-async def visual_task(q: asyncio.Queue, arr_reference: MutableSequence):
+async def visual_task(q: asyncio.Queue, arr_reference: MutableSequence, visualize):
     """
     Task dealing with output formatting.
     :param q:
     :param arr_reference: Pass reference of currently sorted array.
+    :param visualize: Function to draw
     """
 
     largest_digit = len(str(max(arr_reference)))
@@ -81,9 +96,9 @@ async def visual_task(q: asyncio.Queue, arr_reference: MutableSequence):
         except TypeError:
             break
         else:
-            inline(access, write, color_func_map, frame, largest_digit)
+            visualize(access, write, color_func_map, frame, largest_digit)
 
-        await asyncio.sleep(0.03)
+        await asyncio.sleep(0.1)
 
 
 async def run_sort(sort_func, arr: ArrayWrap):
@@ -91,13 +106,13 @@ async def run_sort(sort_func, arr: ArrayWrap):
     await arr.queue.put(None)  # end_val
 
 
-async def sort_main(sort_list, test: MutableSequence):
+async def sort_main(sort_list, test: MutableSequence, visualizer):
     for sort_type in sort_list:
         print(sort_type.__name__)
         steps_queue = asyncio.Queue()
         list_object = ArrayWrap(test, steps_queue)
 
-        visual = asyncio.create_task(visual_task(steps_queue, list_object))
+        visual = asyncio.create_task(visual_task(steps_queue, list_object, visualizer))
         sort_task = asyncio.create_task(run_sort(sort_type, list_object))
 
         await sort_task
@@ -108,9 +123,10 @@ def main_loop():
 
     while True:
         testcase = generate_test(get_size())
-        sort_list = get_sorts_list()
+        sort_list = select_sorts_list()
+        visualizing_method = select_visualize_method()
 
-        asyncio.run(sort_main(sort_list, testcase))
+        asyncio.run(sort_main(sort_list, testcase, visualizing_method))
 
         break
 
